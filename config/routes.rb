9c333361ad156aac8066a6447bf4473ae9882a78
Jonathan <listedegarde@gmail.com>
2015-01-24 18:32:02 -0500

    Correct routes error

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
-  get 'login'   => 'sessions#new'
-  get 'login'   => 'sessions#create'
-  get 'logout'  => 'sessions#destroy'
+  root   'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 
