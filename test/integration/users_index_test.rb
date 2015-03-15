f3756ede76c6f2817fbaebacb76ab27c7397feb7
Jonathan <listedegarde@gmail.com>
2015-03-15 09:55:26 -0400

    Correct failing tests and finish user edit, update, index and destroy action

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserIndexTest < ActionDispatch::IntegrationTets
+class UserIndexTest < ActionDispatch::IntegrationTest
 
   def setup
     @admin     = users(:michael)
@@ -10,23 +10,23 @@ class UserIndexTest < ActionDispatch::IntegrationTets
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
-    assert_template 'user/index'
+    assert_template 'users/index'
     assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
-    first_page_of_user.each do |user|
+    first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete',
                                                     method: :delete
       end
     end
-    assert_difference 'User.count', -1 d
+    assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
   end
 
   test "index as non-admin" do
-    log_in_as(@non_admin_user)
+    log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
