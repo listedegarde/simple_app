2971065db50f580c408ccad6e07a4a1b341c68bb
Jonathan <listedegarde@gmail.com>
2015-04-23 23:58:44 -0400

    Start an integration test for password resets

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PasswordResetsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    ActionMailer::Base.deliveries.clear
+    @user = users(:michael)
+  end
+
 end
