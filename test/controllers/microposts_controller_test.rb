5250cd9edbd8742adc301455a5c67f6e9e99ed50
Jonathan <listedegarde@gmail.com>
2015-06-16 17:53:08 -0400

    Correct user mismatch for micropost deletion

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -24,7 +24,7 @@ class MicropostsControllerTest < ActionController::TestCase
     log_in_as(users(:michael))
     micropost = microposts(:ants)
     assert_no_difference 'Micropost.count' do
-      delete :destory, id: micropost
+      delete :destroy, id: micropost
     end
     assert_redirected_to root_url
   end
